bf448b7ac7a0beb703b30de3b146e90cd809d636
Dominic Mazza <dominicfmazza@gmail.com>
2024-07-21 20:33:09 -0600

    fix: telescope commands updated
    fix: neotree file correctly

--- lua/mappings/telescope.lua
+++ lua/mappings/telescope.lua
@@ -1,25 +1,28 @@
 -- Telescope Settings
 local wk = require("which-key")
 
-wk.register({
-        f = {
-            name = "+telescope",
-            f = { require("telescope.builtin").find_files, "Find Files" },
-            t = { "<cmd>TodoTelescope<cr>","Find TODOs" },
-            g = { require("telescope.builtin").live_grep, "Live Grep" },
-            b = { require("telescope.builtin").buffers, "Buffers" },
-            h = { require("telescope.builtin").help_tags, "Help Tags" },
-            ["/"] = { function()
-                require("telescope.builtin").current_buffer_fuzzy_find()
-            end, "Current Buffer" },
-            w = { function()
-                require("telescope.builtin").grep_string()
-            end, "Find word under cursor" },
-            k = { "<cmd>Legendary<cr>", "Find Keybinds" },
-        }
+wk.add({
+    { "<leader>f",  group = "+telescope", },
+    { "<leader>ff", require("telescope.builtin").find_files, desc = "Find Files" },
+    { "<leader>ft", "<cmd>TodoTelescope<cr>",                desc = "Find TODOs" },
+    { "<leader>fg", require("telescope.builtin").live_grep,  desc = "Live Grep" },
+    { "<leader>fb", require("telescope.builtin").buffers,    desc = "Buffers" },
+    { "<leader>fh", require("telescope.builtin").help_tags,  desc = "Help Tags" },
+    {
+        "<leader>f/",
+        function()
+            require("telescope.builtin").current_buffer_fuzzy_find()
+        end,
+        desc = "Current Buffer"
+    },
+    {
+        "<leader>fw",
+        function()
+            require("telescope.builtin").grep_string()
+        end,
+        desc = "Find word under cursor"
     },
     {
         noremap = true,
-        prefix = "<leader>"
     }
-)
+})
