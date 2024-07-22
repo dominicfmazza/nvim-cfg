7edfd8a6190eff4d55e33831877003fff040ab74
Dominic Mazza <dominicfmazza@gmail.com>
2024-07-21 19:54:35 -0600

    fix: cmake mappings updated to new which-key spec

--- lua/mappings/term/build.lua
+++ lua/mappings/term/build.lua
@@ -1,58 +1,50 @@
 local wk = require "which-key"
-
-wk.register({
-  m = {
-    name = "+build",
-    b = {
-      function()
-        require("nvchad.term").toggle {
-          pos = "float",
-          id = "cmaketerm",
-          cmd = "cmake --build build -j; exit",
-          clear_cmd = false,
-        }
-      end,
-      "CMake: Build",
-    },
-    d = {
-      function()
-        require("nvchad.term").toggle {
-          pos = "float",
-          id = "cmaketerm",
-          cmd = "cmake -B build -S . -DCMAKE_BUILD_TYPE=Debug; exit",
-        }
-      end,
-      "CMake: Configure Debug",
-    },
-    r = {
-      function()
-        require("nvchad.term").toggle {
-          pos = "float",
-          id = "cmaketerm",
-          cmd = "cmake -B build -S . -DCMAKE_BUILD_TYPE=Release; exit",
-        }
-      end,
-      "CMake: Configure Release",
-    },
-    p = {
-      function()
-        require("nvchad.term").toggle {
-          pos = "float",
-          id = "cmaketerm",
-          cmd = "cd docs && doxygen; exit",
-        }
-      end,
-      "Doxygen: Build Doxygen Docs",
-    },
-    c = {
-      function()
-        require("nvchad.term").toggle {
-          pos = "float",
-          id = "cmaketerm",
-          cmd = "rm -rf ./build/; exit",
-        }
-      end,
-      "CMake: Clean ./build/",
-    },
+wk.add({
+  { "<leader>m",    group = "+build" },
+  {
+    "<leader>mb",
+    function()
+      require("nvchad.term").toggle {
+        pos = "float",
+        id = "cmaketerm",
+        cmd = "cmake --build build -j; exit",
+        clear_cmd = false,
+      }
+    end,
+    desc = "CMake: Build",
   },
-}, { noremap = true, silent = true, prefix = "<leader>" })
+  {
+    "<leader>md",
+    function()
+      require("nvchad.term").toggle {
+        pos = "float",
+        id = "cmaketerm",
+        cmd = "cmake -B build -S . -DCMAKE_BUILD_TYPE=Debug; exit",
+      }
+    end,
+    desc = "CMake: Configure Debug",
+  },
+  {
+    "<leader>mr",
+    function()
+      require("nvchad.term").toggle {
+        pos = "float",
+        id = "cmaketerm",
+        cmd = "cmake -B build -S . -DCMAKE_BUILD_TYPE=Release; exit",
+      }
+    end,
+    desc = "CMake: Configure Release",
+  },
+  {
+    "<leader>mc",
+    function()
+      require("nvchad.term").toggle {
+        pos = "float",
+        id = "cmaketerm",
+        cmd = "rm -rf ./build/; exit",
+      }
+    end,
+    desc = "CMake: Clean ./build/",
+  },
+  { noremap = true, silent = true, }
+})
